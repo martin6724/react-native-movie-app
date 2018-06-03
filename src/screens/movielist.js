fc97db0e4bc2e5f619e1b06b31b5d8bfa562841f
martin6724 <martin.michelleann@gmail.com>
2018-06-02 20:01:55 -0400

    updating services api call

--- src/screens/movielist.js
+++ src/screens/movielist.js
@@ -1,29 +1,86 @@
-/**
- * Sample React Native App
- */
-
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View
+ Platform,
+ StyleSheet,
+ Text,
+ FlatList,
+ View,
+ ScrollView,
+ LayoutAnimation
 } from 'react-native';
 
-// import {ORANGE, PINK, WHITE} from '../../styles';
+
+import LinearGradient from 'react-native-linear-gradient';
+import {ORANGE, PINK, WHITE, GREYBG} from '../../styles';
+import UpcomingList from '../../components/upcomingList';
+import NowListItem from '../../components/NowListItem';
+import Services from '../services';
 
 export default class MovieList extends Component {
-  render() {
-    return (
-      <View style={styles.container}/>
-    );
-  }
+
+ state = {
+   upcoming: [
+     {id:0, title: ''},
+     {id:1, title: ''},
+     {id:2, title: ''}
+   ]
+ }
+componentDidMount() {
+  Services.getUpcomingMovies().then(response => {
+    this.setState({upcoming: response.results});
+  })
+}
+
+ componentWillUpdate(){
+   LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+ }
+
+ keyExtractor = item => item.id;
+
+ renderNowPlaying = ({item}) => <NowListItem data={item} />
+
+ renderUpcoming = ({item}) => <UpcomingList data={item} />
+
+
+ render() {
+   return (
+     <ScrollView style={styles.container}>
+       <Text style={styles.title}>MOVIES</Text>
+       <FlatList
+         data={this.state.upcoming}
+         extraData={this.state}
+         keyExtractor={this.keyExtractor}
+         renderItem={this.renderUpcoming}
+         horizontal
+       />
+       <View style={styles.listcontainer}>
+       <Text>NOW PLAYING</Text>
+       <FlatList
+         data={this.state.upcoming}
+         extraData={this.state}
+         keyExtractor={this.keyExtractor}
+         renderItem={this.renderNowPlaying}
+         horizontal
+         />
+     </View>
+     </ScrollView>
+   );
+ }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
+
+ container: {
+   flex: 1,
+   backgroundColor: GREYBG,
+   paddingLeft: 20,
+ },
+
+ title: {
+   fontSize: 24,
+   fontWeight: '600',
+   marginTop: 80,
+ },
+
 });
